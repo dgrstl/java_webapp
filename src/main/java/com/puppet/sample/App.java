920f6e46eb52b3e24f9c80f6afc30847106fc7eb
David Roberts <dgrstl@gmail.com>
2018-05-14 18:14:50 -0500

    Update App.java
    Changed default port to 9998

--- src/main/java/com/puppet/sample/App.java
+++ src/main/java/com/puppet/sample/App.java
@@ -32,7 +32,7 @@ public class App
     try {
       Spark.port(Integer.parseInt(System.getProperty("appPort")));
     } catch (Exception e) {
-      Spark.port(9999);
+      Spark.port(9998);
     }
 
     Spark.threadPool(10, 5, 600);
